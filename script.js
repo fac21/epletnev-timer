7d1c36aaaee63c6f699247d955cf344fd6a1a44a
epletnev <e.a.pletnev@gmail.com>
2021-03-01 19:21:41 +0000

    Updated css file

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const buttonCancel = document.querySelector('.cancel');
 
 form.addEventListener('submit', handleSubmit);
 buttonStart.addEventListener('click', countdown);
-buttonPause.addEventListener('click', countdown); 
+buttonPause.addEventListener('click', countdown);
 buttonCancel.addEventListener('click', handleCancel);
 
 let timerOn = true;
@@ -17,60 +17,64 @@ let time = workTime * 60;
 let min = 0;
 let sec = 0;
 let interval = null;
+let sound = new Audio("http://res.cloudinary.com/hbrwdfccc/video/upload/v1533820266/Bongos.mp3");
 
 function handleSubmit(event) {
-  event.preventDefault();
-  const workValue = event.target.elements.work.value;
-  const breakValue = event.target.elements.break.value;
-  workTime = workValue;
-  breakTime = breakValue;
-  time = workTime * 60;
+    event.preventDefault();
+    const workValue = event.target.elements.work.value;
+    const breakValue = event.target.elements.break.value;
+    workTime = workValue;
+    breakTime = breakValue;
+    time = workTime * 60;
 }
 
 function countdown() {
-  if (timerOn) {
-    timerOn = false;
-    buttonPause.textContent = 'Pause';
-    interval = setInterval(() => {
-  
-      min = Math.floor(time / 60);
-      sec = time % 60;
-      
-      min = min < 10 ? '0' + min : min;
-      sec = sec < 10 ? '0' + sec : sec;
-      
-      timer.innerHTML = `${min}:${sec}`;
-      
-      time--;
-      if (time < 0) {
-           if (work) {
-           time = breakTime * 60;
-           work = false;
-           } else {
-            time = workTime * 60;
-            work = true;
-           }
-        }
-      }, 1000);
-  
+    if (timerOn) {
+        timerOn = false;
+        buttonPause.textContent = 'Pause';
+        interval = setInterval(() => {
+
+            min = Math.floor(time / 60);
+            sec = time % 60;
+
+            min = min < 10 ? '0' + min : min;
+            sec = sec < 10 ? '0' + sec : sec;
+
+            timer.innerHTML = `${min}:${sec}`;
+
+            time--;
+            if (time < 0) {
+                if (work) {
+                    time = breakTime * 60;
+                    work = false;
+                    sound.play();
+                    document.body.style.background = "linear-gradient(to right, rgb(0, 255, 251), rgb(0, 255, 98))";
+                } else {
+                    time = workTime * 60;
+                    work = true;
+                    sound.play();
+                    document.body.style.background = "linear-gradient(to right, rgb(255, 200, 0), rgb(255, 0, 0))";
+                }
+            }
+        }, 1000);
+
     } else {
-      clearInterval(interval);
-      timerOn = true;
-      buttonPause.textContent = 'Start';
+        clearInterval(interval);
+        timerOn = true;
+        buttonPause.textContent = 'Start';
     }
 }
 
 function handleCancel() {
-clearInterval(interval);
-min = 0;
-sec = 0;
-min = min < 10 ? '0' + min : min;
-sec = sec < 10 ? '0' + sec : sec;
-timer.innerHTML = `${min}:${sec}`;
-time = workTime * 60;
-work = true;
-timerOn = true;
-form.reset()
+    clearInterval(interval);
+    min = 0;
+    sec = 0;
+    min = min < 10 ? '0' + min : min;
+    sec = sec < 10 ? '0' + sec : sec;
+    timer.innerHTML = `${min}:${sec}`;
+    time = workTime * 60;
+    work = true;
+    timerOn = true;
+    form.reset()
+    document.body.style.background = "linear-gradient(to right, rgb(255, 200, 0), rgb(255, 0, 0))";
 }
-
-
